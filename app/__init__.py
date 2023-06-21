e44e3590fa6dd46400bd653da3b74c28ab620eb5
blackBoxMod <davis.gamer07@gmail.com>
2023-06-21 11:18:46 -0400

    Adding components 1

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask import request
 from flask_restful import Api
 
 genaibox = Flask(__name__)
@@ -7,3 +8,16 @@ api = Api(genaibox, prefix='/api')
 @genaibox.route("/")
 def index():
     return render_template("index.html")
+
+
+@genaibox.route('/process', methods=['POST'])
+def process():
+    user_input = request.form['user_input']
+
+    # Make the API call using the user input
+    # ...
+
+    # Process the API response
+    # ...
+
+    return 'API call successful!'
